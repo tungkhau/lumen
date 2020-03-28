ad8cec7af794d1889b7f44bd92d92f6a317a96ab
AN <nguyen.hong.an.ktk@gmail.com>
2020-03-28 13:14:45 +0700

    user test case

---
+++ tests/UserTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testCreate()
+    {
+        $inputs = ['user_id' => '545454',
+            'user_name' => 'AN',
+            'role' => 'merchandiser',
+            'workplace_pk' => '38eced6a-6dd8-11ea-bc55-0242ac130003'];
+        $data = ['id' => '545454',
+            'name' => 'AN',
+            'role' => 'merchandiser',
+            'workplace_pk' => '38eced6a-6dd8-11ea-bc55-0242ac130003',
+            'password' => app('hash')->make(env('DEFAULT_PASSWORD'))];
+        $this->call('POST','create_user',$inputs);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('users',$data);
+    }
+    public function testDeactivate ()
+    {
+        $inputs = ['user_pk' => '511f4482-6dd8-11ea-bc55-0242ac130003'];
+        $data = ['pk' => '511f4482-6dd8-11ea-bc55-0242ac130003',
+            'is_active' => False ];
+        $this->call('PATCH','deactivate_user',$inputs);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('users',$data);
+    }
+    public function testReactivate ()
+    {
+        $inputs = ['user_pk' => '59a67242-6dd8-11ea-bc55-0242ac130003'];
+        $data = ['user_pk' => '59a67242-6dd8-11ea-bc55-0242ac130003',
+            'is_active' => True ];
+        $this->call('PATCH','reactivate_user',$inputs);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('users',$data);
+    }
+    public function testResetPassword ()
+    {
+        $inputs = ['user_pk' => '511f4482-6dd8-11ea-bc55-0242ac130003',];
+        $data = ['pk' => '511f4482-6dd8-11ea-bc55-0242ac130003',
+            'password' => app('hash')->make(env('DEFAULT_PASSWORD'))];
+        $this->call('PATCH','reset_user_password',$inputs);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('users',$data);
+    }
+    public function testChangeWorkPlace ()
+    {
+        $inputs = ['user_pk' => '511f4482-6dd8-11ea-bc55-0242ac130003',
+            'workplace_pk' => '38eced6a-6dd8-11ea-bc55-0242ac130003'];
+        $data = ['user_pk' => '511f4482-6dd8-11ea-bc55-0242ac130003',
+            'workplace_pk' => 'cdbe8122-70b9-11ea-bc55-0242ac130003'];
+        $this->call('PATCH','change_user_workplace',$inputs);
+        $this->seeStatusCode(200);
+        $this->seeInDatabase('users', $data);
+    }
+}
