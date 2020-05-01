8e6e2415c1c7d5fca15eba433fb9916cc3d3fab6
AN <nguyen.hong.an.ktk@gmail.com>
2020-05-01 09:16:10 +0700

    back up before arrange seeding data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call('WorkplacesTableSeeder');
-//        $this->call('CustomersTableSeeder');
-//        $this->call('SuppliersTableSeeder');
-//        $this->call('UsersTableSeeder');
-//        $this->call('BlocksTableSeeder');
-//        $this->call('TypesTableSeeder');
-//        $this->call('UnitsTableSeeder');
-//        $this->call('SitesTableSeeder');
-//        $this->call('AccessoriesTableSeeder');
+        $this->call('WorkplacesTableSeeder');
+        $this->call('CustomersTableSeeder');
+        $this->call('SuppliersTableSeeder');
+        $this->call('UsersTableSeeder');
+        $this->call('BlocksTableSeeder');
+        $this->call('TypesTableSeeder');
+        $this->call('UnitsTableSeeder');
+        $this->call('SitesTableSeeder');
+        $this->call('AccessoriesTableSeeder');
         $this->call('TestingDataSeeder');
 
     }
